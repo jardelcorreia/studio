415f2270d3d949635dfaa02c2420f0381b03efd7
Jardel Correia <jardel.lc@gmail.com>
2026-03-02 17:13:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7344387368-26e1e",
+  "appId": "1:276111019128:web:e7b5e0343df79e6da0318d",
+  "apiKey": "AIzaSyA6noJTkCcRypfeqi91qa6a2hDEcQ606N0",
+  "authDomain": "studio-7344387368-26e1e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "276111019128"
+};
